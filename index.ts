e3940a3f3f49eee3074f12c2f6a34b5435e26c8e
Shadi Ammer Mohammed Sobhy Almohtas <106898290+Shadi-Almohtaseb@users.noreply.github.com>
2023-08-18 23:27:20 +0300

    Update index.ts

--- index.ts
+++ index.ts
@@ -5,7 +5,7 @@ import { loggerMiddleware } from "./middleware/logger.js";
 
 dotenv.config()
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 app.use(express.json())
 
 app.use(loggerMiddleware)
